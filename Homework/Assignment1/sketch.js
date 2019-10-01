d3216be50bfca580fdbeb10f439eee8c94fb8a98
txr399 <txr399@miami.edu>
2019-10-01 00:24:06 -0400

    Files for Assignment 1

---
+++ Homework/Assignment1/sketch.js
@@ -0,0 +1,42 @@
+var rectWidth = 150;
+var triEyeYone = 175;
+var triEyeYtwo = 195;
+var eyeColor = ('red');
+var noseColor = (216, 34, 100);
+var mouthColor = ('pink');
+var faceColor = ('orange');
+//each of these variables provides substitutions for position and color
+
+function setup() {
+createCanvas(500,500);
+background(255,255,0);
+//colors the background
+
+}
+
+function draw() {
+
+rectMode(CENTER);
+//puts the rectangle in he middle of the canvas
+
+fill(faceColor);
+rect (250, 250, rectWidth, 200);
+//create the face
+
+fill(noseColor);
+triangle (248, 265, 238, 235, 258, 235);
+//nose triangle
+
+fill(mouthColor);
+rect (250, 300, 105, 25);
+//rectangle mouth
+
+fill(eyeColor);
+triangle (215, triEyeYone, 205, triEyeYtwo, 225, triEyeYtwo);
+//first eye triangle
+
+triangle (280, triEyeYone, 270, triEyeYtwo, 290, triEyeYtwo);
+//second eye triangle
+
+
+}
